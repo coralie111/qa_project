677242bad8f4fc48a1f9668f5cf4e975c1c9c368
William Dang <willi.am.publi@gmail.com>
2020-03-05 19:17:48 +0100

    Change path in data_viz.py and clean comments

--- dataviz/data_viz.py
+++ dataviz/data_viz.py
@@ -1,11 +1,10 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.set()
 
-train = pd.read_csv('Data/train.csv')
+train = pd.read_csv('../data/train.csv')
 
 train.info()
 
@@ -19,8 +18,6 @@ g = sns.countplot('host', data=train, order=train.host.value_counts().iloc[:6].i
 g.set_xticklabels(g.get_xticklabels(), rotation=30)
 g.set_title("instance's category distribution")
 plt.show()
-
-# Distribution des valeurs des variables cibles
 
 train_d = train.copy()
 targets = list(train.columns[11:])
@@ -62,44 +59,3 @@ for i in range(0, len(targets_question), chunk_size):
                     data=train_d_m[train_d_m.variable.isin(targets_question[i:i + chunk_size])],
                     kind='count')
     plt.show()
-
-# fig = plt.figure()
-
-# for i in list(range(30)):
-#     fig.add_subplot(30, 1, i+1)
-#     sns.catplot(x=targets[i], data=train_d, kind='count')
-#     plt.title(targets[i])
-# plt.show()
-
-# # En fonction des catégories
-
-# fig = plt.figure()
-
-# for i in list(range(30)):
-#     fig.add_subplot(30, 1, i+1)
-#     sns.catplot(x=targets[i], col='category', data=train_d, kind='count')
-#     plt.title(targets[i])
-#     plt.show()
-
-# # Valeurs des variables cibles (question/answer) en fonction des catégories
-
-# targets_question = list(train.columns[11:32])
-# targets_answer = list(train.columns[33:])
-
-# fig = plt.figure(figsize = (7, 200))
-
-# for i in list(range(len(targets_question))):
-#     fig.add_subplot(30, 1, i+1)
-#     sns.catplot(x='category', y=targets_question[i], kind='bar', data=train)
-#     plt.show()
-
-# fig2= plt.figure(figsize = (7, 200))
-
-# for i in list(range(len(targets_answer))):
-#     fig2.add_subplot(30, 1, i+1)
-#     sns.catplot(x='category', y=targets_answer[i], kind='bar', data=train)
-#     plt.show()
-
-
-
-
