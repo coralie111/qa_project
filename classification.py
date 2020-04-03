b9db7ecfd2a976d8a2f5be354cb8e1c0e8075df7
William Dang <willi.am.publi@gmail.com>
2020-04-03 17:28:07 +0200

    Add few new features

--- classification.py
+++ classification.py
@@ -2,46 +2,97 @@ import sklearn
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.sparse as sp
-from sklearn.feature_extraction.text import CountVectorizer
+from paths import joblib_dir
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.model_selection import train_test_split
-
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 train = pd.read_csv('data/train.csv')
+
+y = train.iloc[:, 11:]
+
+# transformation des targets variables catégorielles
+y_transformed = y.apply(lambda x: pd.cut(x,
+                        [-0.1, .25, .5, .75, 1.1],
+                        labels=['low', 'medium-', 'medium+', 'high']))
+
+# séparation en cas d'études séparés sur les questions ou answers
+y_question = y_transformed.loc[:, y_transformed.columns.str.startswith('question')]
+y_answer = y_transformed.loc[:, y_transformed.columns.str.startswith('answer')]
+
 
 to_delete_var = ['qa_id', 'url',
                  'question_user_name', 'question_user_page',
                  'answer_user_name', 'answer_user_page']
 
-X_train = train.iloc[:, :11].drop(to_delete_var, 1)
-y_train = train.iloc[:, 11:]
+X = train.iloc[:, :11].drop(to_delete_var, 1)
+X_title = train.question_title
+X_question = train.question_body
+X_answer = train.answer
 
-y_train_transformed = y_train.apply(lambda x: pd.cut(x,
-                                                     [-0.1, .25, .5, .75, 1.1],
-                                                     labels=['low', 'medium-', 'medium+', 'high']))
+# TF-IDF transformer sans stop-words pour l'instant
+answer_tfidftransformer = TfidfVectorizer()
 
-vectorizer = CountVectorizer(stop_words='english',
-                             lowercase=True,
-                             ngram_range=(1, 2))
+# fit sur les answers (possède plus de vocabulaire)
+answer_tfidftransformed = answer_tfidftransformer.fit_transform(X_answer)
+
+# transformation sur le titre et les answers
+question_tfidftransformed = answer_tfidftransformer.transform(X_question)
+title_tfidftransformed = answer_tfidftransformer.transform(X_title)
+
+# degré de proximité entre title/answer et question/answer
+title_answer_similarity = cosine_similarity(title_tfidftransformed, answer_tfidftransformed).diagonal()
+question_answer_similarity = cosine_similarity(question_tfidftransformed, answer_tfidftransformed).diagonal()
+
+try:
+    question_acp = joblib.load(joblib_dir+'question_pca.joblib')
+    answer_acp = joblib.load(joblib_dir+'answer_pca.joblib')
+except FileNotFoundError:
+    raise('jblib file not found, run script first')
+
+question_tfidftransformed_acp = question_acp.transform(question_tfidftransformed)
+answer_tfidftransformed_acp = answer_acp.transform(answer_tfidftransformed)
+
+# extraction du nombre de lignes avec passage à la ligne comme proxy
+linebreak_re = re.compile('\\n')
+question_nblines = X_question.apply(lambda x: len(linebreak_re.findall(x)))
+answer_nblines = X_question.apply(lambda x: len(linebreak_re.findall(x)))
+
+# extraction du nombre de la longueur/verbosité avec nombre de caractères comme proxy
+question_nbchars = X_question.apply(lambda x: len(x))
+answer_nbchars = X_answer.apply(lambda x: len(x))
+
+# OHE avec drop first et au format sparse
 ohe = OneHotEncoder(drop='first', sparse=True)
 
-text_var = ['question_title', 'question_body', 'answer']
+X_category = ohe.fit_transform(X[['category', 'host']])
 
-X_train_text = sp.hstack(X_train[text_var].apply(lambda col: vectorizer.fit_transform(col)))
-X_train_category = ohe.fit_transform(X_train[['category', 'host']])
-X_train_transformed = sp.hstack([X_train_text, X_train_category])
 
-X_train_train, X_train_test, y_train_train, y_train_test = train_test_split(X_train_transformed,
-                                                                            y_train_transformed,
-                                                                            test_size=0.15)
+X_transformed = sp.hstack([question_tfidftransformed_acp, 
+                           answer_tfidftransformed_acp,
+                           X_category])
+
+X_train, X_test, y_train, y_test = train_test_split(X_transformed,
+                                                    y_transformed,
+                                                    test_size=0.15)
 
 dtc = DecisionTreeClassifier(max_depth=5)
 dtc_multi = MultiOutputClassifier(dtc, n_jobs=-1)
 
-dtc_multi.fit(X_train_train, y_train_train)
-dtc_multi.score(X_train_test, y_train_test)
+dtc_multi.fit(X_train, y_train)
+dtc_multi.score(X_test, y_test)
+
+def transform_split_score(X, y):
+    if isinstance(X, list):
+        X_transformed = sp.hstack(X[text_var].apply(lambda col: vectorizer.fit_transform(col)))
+    X_transformed = vectorizer.fit_transform(X)
+    X_train, X_test, y_train, y_test = train_test_split(X_transformed,
+                                                        y,
+                                                        test_size=0.15)
+    dtc_multi.fit(X_train, y_train)
+    return dtc_multi.score(X_test, y_test)
 
 # tree interpretation
 
